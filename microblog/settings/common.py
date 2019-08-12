75ceddf0a53c9d00a06eabb1d180e5645bb7b904
hydoro <hydoro-3181@itstudio.co>
2019-08-12 16:45:57 +0900

    Heroku対応

--- microblog/settings/common.py
+++ microblog/settings/common.py
@@ -19,9 +19,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%19olbl@y(yec=#0p(f_f_(f+)uup16-8$gdh!=@90--3q*v_a'
-
+try:
+    from os import environ
+    SECRET_KEY = environ['SECRET_KEY']
+except ImportError:
+    pass
 
 
 ALLOWED_HOSTS = []
